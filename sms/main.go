ab915af388fc16e9046485e5602838bffb706099
fuadop <fuadolatunji@gmail.com>
2022-03-12 18:45:52 +0100

    fix: fix panic error (parse json payload the right way)

--- sms/main.go
+++ sms/main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"strings"
-	"net/http"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
 
-	url := "https://api.sendchamp.com//api/v1/sms/send"
+	url := "https://api.sendchamp.com/api/v1/sms/send"
 
-	payload := strings.NewReader("{
-    "to": [
-        "2348188469520"
-    ],
-    "message": "Lorem ipsum d, no lele",
-    "sender_name": "Dash",
-    "route": "non_dnd"
-}")
+	type SendSMSPayload struct {
+		To         []string `json:"to"`
+		Message    string   `json:"message"`
+		SenderName string   `json:"sender_name"`
+		Route      string   `json:"route"`
+	}
+
+	sp := SendSMSPayload{
+		To:         []string{"2348188469520"},
+		Message:    "Lorem ipsum d, no lele",
+		SenderName: "ASF",
+		Route:      "international",
+	}
+
+	j, _ := json.Marshal(sp)
+	payload := bytes.NewReader(j)
 
 	req, _ := http.NewRequest("POST", url, payload)
 
@@ -26,7 +36,11 @@ func main() {
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer ACCESS_KEY")
 
-	res, _ := http.DefaultClient.Do(req)
+	res, e := http.DefaultClient.Do(req)
+
+	if e != nil {
+		log.Fatal(e)
+	}
 
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
